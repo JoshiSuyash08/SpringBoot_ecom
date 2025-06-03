3bfd10c6f8fe2262e193e686d763e78735bed050
Suyash Joshi <suyashjoshi088@gmail.com>
2025-06-03 18:12:50 -0400

    add ResponseEntity

--- src/main/java/com/sb/ecomspringboot/controller/ProductController.java
+++ src/main/java/com/sb/ecomspringboot/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.sb.ecomspringboot.controller;
 import com.sb.ecomspringboot.model.Product;
 import com.sb.ecomspringboot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,13 +18,19 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet() {
-        return "Hello suyash";
-    }
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/com/sb/ecomspringboot/model/Product.java
+++ src/main/java/com/sb/ecomspringboot/model/Product.java
@@ -1,5 +1,6 @@
 package com.sb.ecomspringboot.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +29,8 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date releaseDate;
     private boolean available;
     private int quantity;
